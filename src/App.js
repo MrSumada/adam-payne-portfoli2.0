1d33bb12ad7630d5ee3b26e5bf404b0d1095d538
MrSumada <ricksamericana.com>
2022-05-01 18:08:34 -0700

    add navbar clicks

--- src/App.js
+++ src/App.js
@@ -10,23 +10,41 @@ function App() {
 
   const [categories] = useState([
     {
-      name: 'about me',
+      name: 'About Me',
       description: 'A description of web designer, Adam Payne',
     },
-    { name: 'portfolio', description: 'Portraits of people in my life' },
-    { name: 'resume', description: 'Delicious delicacies' },
-    { name: 'contact', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: 'Portfolio', description: 'Some projects from Adam Payne' },
+    { name: 'Resume', description:"'Adam's skills" },
+    { name: 'Contact', description: 'Reach out to Adam' },
   ]);
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [currentPage, setCurrentPage] = useState('About');
 
-  const [contactSelected, setContactSelected] = useState(false);
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    // if (currentPage === 'Projects') {
+    //   return <Projects />;
+    // }
+    // if (currentPage === 'Resume') {
+    //   return <Resume />;
+    // }
+    // return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
 
   return (
     <div className='container'>
-      <Nav />
+      <Nav 
+        categories={categories}
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
       <main>
-        <About />
+        {renderPage()}
       </main>
       <Footer />
     </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,18 +3,44 @@ import React from "react";
 function Nav(props) {
     const {
         categories = [],
-        CurrentCategory,
-        setCurrentCategory
+        currentPage,
+        setCurrentPage,
+        handlePageChange
     } = props;
 
     return (
         <header className="flex-row px-2">
             <a href="/"><h2 className="px-2">Adam Payne</h2></a>
             <nav className="flex-row">
-                <a href="/"><h3 className="px-2">About Me</h3></a>
-                <a href="/"><h3 className="px-2">Portfolio</h3></a>
-                <a href="/"><h3 className="px-2">Contact</h3></a>
-                <a href="/"><h3 className="px-2">Resume</h3></a>
+                <a
+                    href="#about"
+                    onClick={() => handlePageChange('About')}
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                    About
+                </a>
+                <a
+                    href="#projects"
+                    onClick={() => handlePageChange('Projects')}
+                    className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+                >
+                    Projects
+                </a>
+                <a
+                    href="#resume"
+                    onClick={() => handlePageChange('Resume')}
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </a>
+                <a
+                    href="#Contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </a>
+                
             </nav>
             
         </header>
